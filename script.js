a2a2be2b3ea1a997336be984a3dd454d04b87938
Kate <dgmk9-k@yahoo.com>
2021-02-20 18:46:03 +0900

    interface changes

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-var navigation = getElementById("navigation")
+var navigation = document.getElementById("navigation");
+var topHeader = document.getElementById("topHeader");
+
+
+window.onscroll = function(){
+    if(document.body.scrollTop > 70 || document.documentElement.scrollTop > 70){
+        navigation.classList.remove("pleaseStay");
+        navigation.classList.add("scrollWithMe");
+    } else{
+        navigation.classList.add("pleaseStay");
+        navigation.classList.remove("scrollWithMe");
+    }
+}
