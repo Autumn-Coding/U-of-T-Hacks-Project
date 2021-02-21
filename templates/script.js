8150901ee63fcd9ded9dd11fd977731511bee0df
Autumn-Coding <78398232+Autumn-Coding@users.noreply.github.com>
2021-02-21 22:34:43 +0900

    Update script.js

--- templates/script.js
+++ templates/script.js
@@ -19,7 +19,10 @@ window.onscroll = function(){
 var formData = new FormData();
 
 function thisIsLoading() {
-    addRoom.innerHTML="<img src='upload.wikimedia.org/wikipedia/commons/9/92/Loading_icon_cropped.gif'>";
+    var loadingImage = document.createElement("img");
+    loadingImage.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/a/ad/YouTube_loading_symbol_3_%28transparent%29.gif");
+    addRoom.innerHTML = '';
+    addRoom.appendChild(loadingImage);
 }
 
 function collectImage() {
